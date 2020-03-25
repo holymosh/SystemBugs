aa2085a57408bfc09b1b0f7a2a9e82fd8b91e1b5
holymosh <dpopov1997@gmail.com>
2020-03-26 00:41:20 +0300

    domain base entity

---
+++ SystemBugsBackend/SystemBugsBackend/DTO/BugDto.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace SystemBugsBackend.DTO
+{
+    public class BugDto
+    {
+        [JsonPropertyName("ID")]
+        public int Id { get; set; }
+
+        public string System { get; set; }
+        public string Summary { get; set; }
+
+        [JsonPropertyName("Состояние")]
+        public string State { get; set; }
+
+    }
+}
