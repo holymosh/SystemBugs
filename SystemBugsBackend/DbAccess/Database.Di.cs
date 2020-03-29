1ca34c42ac900d27d8a8c7a5bb68d6bffcdff08a
holymosh <dpopov1997@gmail.com>
2020-03-29 13:04:42 +0300

    db access register

---
+++ SystemBugsBackend/DbAccess/Database.Di.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace DbAccess
+{
+    public static class DatabaseDI
+    {
+        public static void RegisterDb(this IServiceCollection serviceCollection)
+        {
+            var connectionString =
+                @"Server=.\SQLExpress;Database=myDataBase;Integrated Security=True;Asynchronous Processing=True;";
+            serviceCollection.AddEntityFrameworkSqlServer().AddDbContext<DomainDbContext>(builder => builder.UseSqlServer(connectionString));
+
+            serviceCollection.AddScoped(typeof(IGenericRepository<>),typeof(EfGenericRepository<>));
+            
+
+        }
+    }
+}
--- SystemBugsBackend/SystemBugsBackend/Startup.cs
+++ SystemBugsBackend/SystemBugsBackend/Startup.cs
@@ -7,6 +7,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using static DbAccess.DatabaseDI;
 
 namespace SystemBugsBackend
 {
@@ -23,6 +24,7 @@ namespace SystemBugsBackend
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllersWithViews();
+            services.RegisterDb();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
