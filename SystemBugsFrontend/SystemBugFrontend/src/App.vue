a5308304a22e2576569cd965d18c8b318b7f563d
holymosh <dpopov1997@gmail.com>
2020-03-30 13:11:42 +0300

    vue js front

---
+++ SystemBugsFrontend/SystemBugFrontend/src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <div id="app">
+    <BugComponent/>
+  </div>
+</template>
+
+<script>
+import BugComponent from './components/BugComponent'
+
+export default {
+  name: 'App',
+  components: {
+    BugComponent
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
