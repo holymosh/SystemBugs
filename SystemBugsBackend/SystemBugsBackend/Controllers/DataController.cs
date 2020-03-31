89ad8562538543b170d6ec8265a0484a772340cd
holymosh <dpopov1997@gmail.com>
2020-03-31 22:07:48 +0300

    GetData

---
+++ SystemBugsBackend/SystemBugsBackend/Controllers/DataController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Linq;
+using DbAccess;
+using Domain.Entities;
+using Microsoft.AspNetCore.Mvc;
+
+namespace SystemBugsBackend.Controllers
+{
+    [Route("Data")]
+    public class DataController : ControllerBase
+    {
+        private readonly IRepository<Bug> _repository;
+
+        public DataController(IRepository<Bug> repository)
+        {
+            _repository = repository;
+        }
+
+        [HttpGet]
+        [Route("get")]
+        public IActionResult GetData([FromQuery] int? systemId, [FromQuery] int? levelId,
+            [FromQuery] DateTime? from ,[FromQuery] DateTime? to)
+        {
+
+            var query = _repository.GetAll();
+            if (systemId.HasValue) query = query.Where(b => b.SystemId == systemId);
+
+            if (levelId.HasValue) query = query.Where(b => b.CriticalLevelId == levelId);
+
+            if (from.HasValue && to.HasValue && from < to)
+                query = query.Where(b => from <= b.CreationDate && b.CreationDate <= to);
+
+            return Ok(query);
+        }
+    }
+}
